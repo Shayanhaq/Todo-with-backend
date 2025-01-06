e34eff655dce3b46f5766b28ce649e769ee54742
shayan haq <haqshayan2@gmail.com>
2025-01-06 18:14:04 +0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html", // Include the main HTML file
+    "./src/**/*.{js,jsx,ts,tsx}", // Include all JS/TS/React files in the src directory
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
